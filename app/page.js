5f566cbeb4c5c99fbd998ba3ef051a38c314db78
setsuwww <setsuprojectonly@gmail.com>
2026-06-07 19:17:18 +0700

    Tracked push

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Hero from "./components/Hero";
-import About from "./components/About";
+import Hero from "./components/content/Hero";
+import About from "./components/content/About";
 import Skills from "./components/Skills";
 
 export default function Home() {
