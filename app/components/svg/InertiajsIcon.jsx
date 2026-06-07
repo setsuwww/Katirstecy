013b4a59762739dd7b53fe3b51dc0b91d6351b72
setsuwww <setsuprojectonly@gmail.com>
2026-06-07 19:33:52 +0700

    Icon new

--- app/components/svg/InertiajsIcon.jsx
+++ app/components/svg/InertiajsIcon.jsx
@@ -2,12 +2,29 @@ import React from "react";
 
 const InertiajsIcon = (props) => (
   <svg
-    viewBox="0 0 24 24"
-    fill="currentColor"
+    width="1em"
+    height="1em"
     xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 128 128"
     {...props}
   >
-    <path d="M12 2L2 7l10 5 10-5-10-5z" />
+    <defs>
+      <linearGradient
+        id="SVGqdAbMSVr"
+        x1="0"
+        x2="128"
+        y1="64"
+        y2="64"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stopColor="#9553e9"></stop>
+        <stop offset="1" stopColor="#6d74ed"></stop>
+      </linearGradient>
+    </defs>
+    <path
+      fill="#000000"
+      d="M92.433 28.433H55.625L91.192 64L55.625 99.567h36.808L128 64zm-55.625 0H0L35.567 64L0 99.567h36.808L72.375 64z"
+    ></path>
   </svg>
 );
 
