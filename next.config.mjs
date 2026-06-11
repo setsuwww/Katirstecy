c90d2dfbe0de6ee2150a09c12202d7b5cd9a2fa5
setsuwww <setsuprojectonly@gmail.com>
2026-06-11 14:40:43 +0700

    PWA Startup

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  turbopack: {},
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
